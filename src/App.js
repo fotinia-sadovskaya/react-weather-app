d8e954b685b4a13fc81d4c4ddae1530f09b29793
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-03-23 21:54:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="" target="_blank" rel="noopener noreferrer">
+          Svitlana Voronko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/fotinia-sadovskaya/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
