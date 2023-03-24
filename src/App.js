5c61f5aed733685fb6145621c8f349c0b761668f
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-03-24 17:35:53 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,29 +5,31 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.behance.net/fotinia_sadovskaya"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Svitlana Voronko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/fotinia-sadovskaya/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a href="" target="_blank" rel="noopener noreferrer">
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.behance.net/fotinia_sadovskaya"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Svitlana Voronko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/fotinia-sadovskaya/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="" target="_blank" rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
