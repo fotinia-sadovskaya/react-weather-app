fb7f19a79689ac8f30d26541a40f1b49f6460f37
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-03-25 16:11:30 +0200

    Changed unit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Dnipro" />
+        <Weather defaultCity="Burgas" />
         <footer>
           This project was coded by{" "}
           <a
