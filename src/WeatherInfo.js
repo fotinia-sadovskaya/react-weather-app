076f238f22e49a5962f984b0e57f93fcd1d611ea
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-03-27 13:09:15 +0300

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,8 +15,14 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-            <WeathrIcon code={props.data.icon} />
-            <WeatherTemperature celsius={props.data.temperature} />  
+          <div className="d-flex">
+            <div>
+              <WeathrIcon code={props.data.icon} />
+            </div>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul className="weather-date">
