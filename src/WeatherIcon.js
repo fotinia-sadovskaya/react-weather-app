c1ea49de8179cbd52b56eed7cfae90654c133197
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-04-04 21:21:57 +0300

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     return (
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
-        color="yellow"
+        color="#fcd720"
         size={props.size}
         animate={true}
       />
