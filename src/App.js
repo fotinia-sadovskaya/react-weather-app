a80d1855de4f44a6a337af7baf28ebe4bb4302a3
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-04-05 11:57:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity={props.data.city} />
+        <Weather defaultCity={city} />
         <Footer />
       </div>
     </div>
