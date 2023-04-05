ec891599b7633caa1f9e4ff6aba1e36143f559ab
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-04-05 12:38:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Weather from "./Weather";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity={city} />
+        <Weather defaultCity="Burgas" />
         <Footer />
       </div>
     </div>
   );
 }
-
-
