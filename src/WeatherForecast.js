838d2bb8c9da32f042a4cfed62a86487d44e60fe
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2024-12-20 15:59:57 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
+    let apiKey = "583543d42b8605a7c28a20d072be705c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
