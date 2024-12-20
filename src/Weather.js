f6f2dd4858152133f5d8182d2cafce1905d40c37
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2024-12-20 16:00:21 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -34,7 +34,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
+    let apiKey = "583543d42b8605a7c28a20d072be705c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
