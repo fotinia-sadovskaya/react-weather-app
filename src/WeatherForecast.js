df2448b35e06e519eb7c1f6d75607942f70ea673
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2025-11-30 16:03:59 +0200

    Refactor API URL to use variable for units

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -21,7 +21,8 @@ export default function WeatherForecast(props) {
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     // let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
